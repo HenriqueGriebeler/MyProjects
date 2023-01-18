c0923db992a538b7f1eda691c96c2cd50d63a8aa
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-01-17 23:28:14 -0300

    Commit do menu

--- HTML5-CSS3/exercicios/IkasRPG/scripts/script.js
+++ HTML5-CSS3/exercicios/IkasRPG/scripts/script.js
@@ -2,6 +2,18 @@ function menu(){
     if(itens.style.display == 'block'){
         itens.style.display = 'none'
     } else {
+        
         itens.style.display = 'block'
+        
     }
+}
+
+function showP(){
+    if(imenu1.style.display == 'block'){
+        imenu1.style.display = 'none'
+    } else {
+    imenu1.style.display = 'block'
+
+    }
+
 }
