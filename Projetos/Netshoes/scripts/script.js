5cdc3a5cc6b5e906e38fd63951ae0518356e4a8a
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-01-24 22:51:45 -0300

    Create the Netshoes Menu

---
+++ Projetos/Netshoes/scripts/script.js
@@ -0,0 +1,20 @@
+mundialMenu = document.getElementById('imundial')
+outletMenu = document.getElementById('ioutlet')
+
+function openMinMenu(){
+    if(mundialMenu.style.display == 'block'){
+        mundialMenu.style.display = 'none'
+    } else {
+        mundialMenu.style.display = 'block'
+        }
+    
+}
+
+function openMinMenuTwo(){
+    if(outletMenu.style.display == 'block'){
+        outletMenu.style.display = 'none'
+    } else {
+        outletMenu.style.display = 'block'
+        }
+    
+} /* DEVO CRIAR O MENU PARA APENAS UM ABRIR POR VEZ, DEVO TAMBÉM COLOCAR OS ICONES DAS CETAS */
