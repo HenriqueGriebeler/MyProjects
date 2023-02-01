662f408ba13f7798a27eb51020751884793a4700
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-01-31 23:05:02 -0300

    Criando nova pag

---
+++ HTML5-CSS3/exercicios/IkasRPGtest/scripts/script.js
@@ -0,0 +1,22 @@
+function menu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        
+        itens.style.display = 'block'
+        
+    }
+}
+
+function showP(){
+    if(imenu1.style.display == 'block'){
+        imenu1.style.display = 'none'
+    } else {
+    imenu1.style.display = 'block'
+
+    }
+
+}
+
+var pagina = 1
+
