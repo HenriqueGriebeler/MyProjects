e3fe67d833e5de48e51b4d832df048d6f62241e8
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-02-02 08:01:32 -0300

    Desenvolvendo
    Criando função de passar imagens dentro do menu

--- Projetos/PagesReader/scripts/script.js
+++ Projetos/PagesReader/scripts/script.js
@@ -8,15 +8,57 @@ function menu(){
     }
 }
 
-function showP(){
-    if(imenu1.style.display == 'block'){
-        imenu1.style.display = 'none'
-    } else {
-    imenu1.style.display = 'block'
 
+
+//COMEÇO DO LEITOR DE PÁGINAS
+
+var pagina = 0
+
+class NewsPaper{
+    constructor(){
+       var arrayPages = []
     }
+       load(){ //FUNÇÃO PRINCIPAL QUE DEVE RODAR
+           this.showP()
+           this.dataReader()
+       }
+       dataReader(){
+        
+    }
+        getPageNumber(){
 
-}
 
-var pagina = 1
 
+            var numbers = document.getElementsByClassName('page')
+            for(let i = 0; i < numbers.length; i++){
+                //AQUI VOU COLOCAR ALGO PARA COMPUTAR DENTRO DO OBJETO
+            // NEWSPAPER PARA COLOCAR A PÁGINA ATUAL COMO ID, SÓ PRECISO
+            // PENSAR MELHOR COMO FAZER ISSO...
+            }
+       }
+    
+      /* currentPage(){  IREI CONFIGURAR ADICIONAR AS IMAGENS NO ARRAYPAGES PARA PODER CONTINUAR ESTE
+
+        for(let i = 0; i < arrayPages.length; i++){
+            var page = document.getElementById('i'+arrayPages[i])
+    
+        }
+       } */ 
+       pageNext(){ /* VOU CONFIGURAR OS BOTÕES AQUI */
+
+       }
+
+       
+       
+       showP(){
+        var page = document.getElementById('imenu1')
+        if(page.style.display == 'block'){
+            page.style.display = 'none'
+        }else{
+            page.style.display = 'block'
+        }
+        }
+   }
+   
+   var newsPaper = new NewsPaper()
+  
