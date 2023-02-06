dffb6f94b111a0023ceb01b21345f7d62a50cb7c
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-02-06 18:44:12 -0300

    kl

--- Projetos/PagesReader/scripts/script.js
+++ Projetos/PagesReader/scripts/script.js
@@ -8,57 +8,31 @@ function menu(){
     }
 }
 
-
-
-//COMEÇO DO LEITOR DE PÁGINAS
-
-var pagina = 0
-
-class NewsPaper{
-    constructor(){
-       var arrayPages = []
+      
+    function load(){
+        showP()
+        countPage()
+   
     }
-       load(){ //FUNÇÃO PRINCIPAL QUE DEVE RODAR
-           this.showP()
-           this.dataReader()
-       }
-       dataReader(){
-        
-    }
-        getPageNumber(){
 
-
-
-            var numbers = document.getElementsByClassName('page')
-            for(let i = 0; i < numbers.length; i++){
-                //AQUI VOU COLOCAR ALGO PARA COMPUTAR DENTRO DO OBJETO
-            // NEWSPAPER PARA COLOCAR A PÁGINA ATUAL COMO ID, SÓ PRECISO
-            // PENSAR MELHOR COMO FAZER ISSO...
-            }
-       }
-    
-      /* currentPage(){  IREI CONFIGURAR ADICIONAR AS IMAGENS NO ARRAYPAGES PARA PODER CONTINUAR ESTE
-
-        for(let i = 0; i < arrayPages.length; i++){
-            var page = document.getElementById('i'+arrayPages[i])
-    
-        }
-       } */ 
-       pageNext(){ /* VOU CONFIGURAR OS BOTÕES AQUI */
-
-       }
-
-       
-       
-       showP(){
+    function showP(){
         var page = document.getElementById('imenu1')
         if(page.style.display == 'block'){
             page.style.display = 'none'
         }else{
             page.style.display = 'block'
         }
-        }
-   }
-   
-   var newsPaper = new NewsPaper()
-  
+        
+    }
+
+    function countPage(){
+
+        //CONTINUAR AMANHA, VOU FAZER MAS SEM SER OBJETO.
+
+    }
+
+    function activePage(){
+
+        //CONTINUAR AMANHA, VOU FAZER MAS SEM SER OBJETO.
+
+    }
