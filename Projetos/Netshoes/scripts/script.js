fd7fc9f26e498bd87ddbe0581cfe557ecde34b61
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-02-09 07:55:58 -0300

    s

--- Projetos/Netshoes/scripts/script.js
+++ Projetos/Netshoes/scripts/script.js
@@ -36,6 +36,69 @@ function openMinMenu(a){
     
 }
 
-
-
 /** FINAL DAS CONFIGUREAÇÕES DO MENU */
+
+function handleImageUpload() {
+
+var image = document.getElementById("upload").files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      document.getElementById("display-image").src = e.target.result;
+    }
+
+    reader.readAsDataURL(image);
+
+} 
+
+function send(){
+
+var productName = document.getElementById('inome')
+var productPrice = document.getElementById('iprice')
+var productImage = document.getElementById('upload')
+var productSRC = document.getElementById('display-image')
+
+let aviso = ''
+
+if(productName.value == ""){
+    aviso += 'Não esqueça de preencher o nome\n'
+}
+if(productPrice.value == ""){
+    aviso += 'Não esqueça de preencher o preço\n'
+}
+if(productImage.value == ""){
+    aviso += 'Não esqueça de enviar uma imagem\n'
+}
+if(aviso != ''){
+    alert(aviso)
+} else if(aviso == ''){
+
+//CONFIGURANDO OS PARAMETROS PARA O OBJETO PRODUTO
+    //FALTA ADICIONAR ELES AO OBJETO AGORA
+produto = new Produto()
+
+//CLEAR NEEDS TO BE THE LAST COMMAND
+clear(productName, productPrice, productImage, productSRC)
+
+}
+}
+
+function clear(a, b, c, d){
+
+    a.value = ''
+    b.value = ''
+    c.value = ''
+    d.src = ''
+
+}
+
+class Produto{
+
+    constructor(){
+        this.productName = ''
+        this.productPrice = ''
+
+    }
+    
+}
