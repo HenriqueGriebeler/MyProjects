2ad32f3282719f758cc547760636e27393a9cb44
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-02-21 00:33:23 -0300

    asd

--- React/ex003/src/App.js
+++ React/ex003/src/App.js
@@ -3,6 +3,9 @@ import "./App.css"
 
 
 function App() {
+
+  {/* 
+
   const [age, setAge] = useState(0)
 
 const increaseAge = () => {
@@ -22,8 +25,28 @@ const resetAge = () => {
 changeText(document.getElementById("AD").value)
 
   }
+  */}
   return (
     <div className="App">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*
+  PROJETO ANTERIOR, COM BOTOES ALTERANDO UM USESTAGE E COPIANDO
+      TUDO DE DENTRO DE UM FORM
+
+
        <button onClick={increaseAge}>Increase age</button><br></br>
        <button onClick={decreaseAge}>Decrease age</button><br></br>
        <button onClick={resetAge}>Reset age</button>
@@ -32,7 +55,7 @@ changeText(document.getElementById("AD").value)
 
        <p><input type="text" onChange={addText} id="AD" size="32"></input></p>
       <p>{texts}</p>
-
+*/}
     </div>
 
     
