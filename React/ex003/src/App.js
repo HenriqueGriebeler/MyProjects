d4bc0ee6bbfccc50e69b1361fe7475ceab399362
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-02-21 00:09:36 -0300

    Studying States in React

---
+++ React/ex003/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import "./App.css"
+
+
+function App() {
+  const [age, setAge] = useState(0)
+
+const increaseAge = () => {
+  setAge(age+1)
+}
+const decreaseAge = () => {
+  setAge(age-1)
+}
+const resetAge = () => {
+  setAge(0)
+
+}
+
+  const [texts, changeText] = useState("")
+
+  const addText = () => {
+changeText(document.getElementById("AD").value)
+
+  }
+  return (
+    <div className="App">
+       <button onClick={increaseAge}>Increase age</button><br></br>
+       <button onClick={decreaseAge}>Decrease age</button><br></br>
+       <button onClick={resetAge}>Reset age</button>
+       <p>{age}</p>
+
+
+       <p><input type="text" onChange={addText} id="AD" size="32"></input></p>
+      <p>{texts}</p>
+
+    </div>
+
+    
+
+
+  );
+}
+
+export default App;
