b6fcbf8cb8b64cfac51414d7c8c45b993c8aa2a0
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-02-27 07:17:21 -0300

    g

--- React/training001/src/App.js
+++ React/training001/src/App.js
@@ -16,7 +16,22 @@ const [Text, setTextColor] = useState('black')
 
 const changeColor = (color) => { 
   setTextColor(color)
-  
+}
+
+const [addText, setAddText] = useState('')
+
+const handleChange = (event) => {
+
+ setAddText(event.target.value)
+
+}
+
+const [submitedArr, setArr] = useState([])
+
+const submit = () => {
+
+setArr(addText) //DEVO ADICIONAR UM .MAP para colcar tudo dentro do array e funcionar
+  console.log(submitedArr)
 }
 
   return (
@@ -30,6 +45,17 @@ const changeColor = (color) => {
         <h2 style={{color:'purple'}} onClick={() => changeColor('purple')}>Purple</h2>
         <h2 style={{color:'pink'}} onClick={() => changeColor('pink')}>Pink</h2>
 
+        </div>
+        <div>
+
+        <h1>Adicionar abaixo:</h1>
+
+        Digite o texto: <input onChange={handleChange}></input>
+          
+        <p>{addText} </p>
+        
+           <p><button onClick={submit}> Submit </button></p>
+           <p>{submitedArr}</p>
         </div>
       </div>
     
