067939177053045f792b1cba171c45f3bf6ce2c5
HenriqueGriebeler <xgamerxhenrique@gmail.com>
2023-03-11 14:12:07 -0300

    dx

--- JavaScript/ex018/script.js
+++
@@ -1,8 +0,0 @@
-a = [0,5,4,7,8,9,4,1,3]
-a.sort()
-for(let i = 0; i < a.length; i++){
-    if(a[i] == 4){
-        console.log('Encontrado')
-    }
-    console.log("A posição "+i+" corresponde ao valor "+a[i])
-}
--- JavaScript/ex020/script.js
+++
@@ -1,9 +0,0 @@
-let amigo = {nome:'Henrique',
-idade:19,
-peso:71,
-treinar(p=0){
-
-Number(this.peso+=p)}}
-
-amigo.treinar(2)
-console.log(amigo.peso)
