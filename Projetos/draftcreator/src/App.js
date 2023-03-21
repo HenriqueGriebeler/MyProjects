8344147f6e9aaa7588435e2d24e72bda2b7a964c
HenriqueGriebeler <henruqiedearajogriebelerr@yahoo.com>
2023-03-21 18:13:21 -0300

    s

--- Projetos/draftcreator/src/App.js
+++ Projetos/draftcreator/src/App.js
@@ -8,36 +8,59 @@ function App() {
 
 
   const [clickedPoints, setClickedPoints] = useState([])
+  const [canPaint, setCanPaint] = useState(true)
+  const [undoPoints, setUndoPoints] = useState([])
 
   const paintClick = (event) => {
 
+    if(canPaint==true){
     const {clientX, clientY} = event
   setClickedPoints([...clickedPoints, {clientX, clientY}])
-  console.log(clickedPoints)
+}
 
 }
-  function undo() {
+  const undo = () => {
     
-    const newClickedPoints = [...clickedPoints]
-    newClickedPoints.pop()
-    setClickedPoints(newClickedPoints)
+    const undoPoint = clickedPoints.pop()
+    setUndoPoints([...undoPoints, undoPoint])
 
+  }
+  const redo = () => {
+
+    if(undoPoints.length > 0){
+    const redoPoint = undoPoints.pop()
+    setClickedPoints([...clickedPoints, redoPoint])
+  }
+
+  }
+
+  const blockPaint = () => {
+    setCanPaint(false)
+  }
+  const freePaint = () => {
+    setCanPaint(true)
   }
 
   return (
 
     <div className="App" onClick={paintClick} style={{color: 'white'}}>
 
-    
+      <div id='paintableArea'>
+
       {clickedPoints.map((clickedPoints) => {
         return <div style={{position: 'absolute', left: clickedPoints.clientX-4, top: clickedPoints.clientY-4, backgroundColor: 'lightgreen', width: '8px', height: '8px', borderRadius: '50%', cursor: 'default'}}></div>
       })}
-
-<button style={{backgroundColor: 'black', border: 'none'}} onClick={undo}><img src={require('./images/undo.png')} style={{cursor: 'pointer'}}></img></button>
-
-<button style={{background: 'black', border: 'none'}}><img src={require('./images/redo.png')} style={{cursor: 'pointer', marginLeft: '2px'}}></img></button>
-
       
+      </div>
+
+<div style={{position: 'absolute', top: '1px'} } onMouseOver={blockPaint} onMouseOut={freePaint}>
+
+  <button style={{backgroundColor: 'black', border: 'none'}} onClick={undo}><img src={require('./images/undo.png')} style={{cursor: 'pointer'}}></img></button>
+  
+  <button style={{background: 'black', border: 'none'}}><img src={require('./images/redo.png')} style={{cursor: 'pointer', marginLeft: '2px'}} onClick={redo}></img></button>
+
+</div>
+
     </div>
   );
 }
