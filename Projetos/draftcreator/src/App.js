614935b6db140473fdae46063a0dd2c6ec32fb73
HenriqueGriebeler <henruqiedearajogriebelerr@yahoo.com>
2023-03-21 23:07:08 -0300

    asd

--- Projetos/draftcreator/src/App.js
+++ Projetos/draftcreator/src/App.js
@@ -1,15 +1,22 @@
-import { useState , useEffect } from 'react'
+import { useState } from 'react'
 import './App.css';
+import { SketchPicker } from 'react-color';
+
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 function App() {
   
-
+  //PONTOS
 
   const [clickedPoints, setClickedPoints] = useState([])
   const [canPaint, setCanPaint] = useState(true)
   const [undoPoints, setUndoPoints] = useState([])
+
+//CORES
+
+  const[color, setColor] = useState('#ffffff')
+  const[colorPicker, showColorPicker] = useState(false)
 
   const paintClick = (event) => {
 
@@ -18,6 +25,19 @@ function App() {
   setClickedPoints([...clickedPoints, {clientX, clientY}])
 }
 
+
+
+}
+
+const ColorPicker = (props) => {
+  return <div id='colorPicker'>
+
+    <SketchPicker 
+        color={props.color}
+        onChange={ (color) => {props.setColor(color.hex)}}
+      />
+  </div>
+  
 }
   const undo = () => {
     
@@ -41,6 +61,10 @@ function App() {
     setCanPaint(true)
   }
 
+  const openColorPicker = () => {
+    colorPicker ? showColorPicker(false) : showColorPicker(true)
+  }
+
   return (
 
     <div className="App" onClick={paintClick} style={{color: 'white'}}>
@@ -48,7 +72,7 @@ function App() {
       <div id='paintableArea'>
 
       {clickedPoints.map((clickedPoints) => {
-        return <div style={{position: 'absolute', left: clickedPoints.clientX-4, top: clickedPoints.clientY-4, backgroundColor: 'lightgreen', width: '8px', height: '8px', borderRadius: '50%', cursor: 'default'}}></div>
+        return <div style={{position: 'absolute', left: clickedPoints.clientX-4, top: clickedPoints.clientY-4, backgroundColor: color, width: '8px', height: '8px', borderRadius: '50%', cursor: 'default'}}></div>
       })}
       
       </div>
@@ -58,6 +82,14 @@ function App() {
   <button style={{backgroundColor: 'black', border: 'none'}} onClick={undo}><img src={require('./images/undo.png')} style={{cursor: 'pointer'}}></img></button>
   
   <button style={{background: 'black', border: 'none'}}><img src={require('./images/redo.png')} style={{cursor: 'pointer', marginLeft: '2px'}} onClick={redo}></img></button>
+
+
+{/* COMEÇO DO SELETOR DE CORES */}
+
+  
+  <button onClick={openColorPicker} style={{verticalAlign: 'top', marginTop: '15px', marginLeft: '20px'}}>Color</button>
+
+  {colorPicker ? <ColorPicker color={color} setColor={setColor}function={openColorPicker}/> : ''}
 
 </div>
 
