9ab12fea17ee488852439d001c282cfacad445d4
HenriqueGriebeler <henruqiedearajogriebelerr@yahoo.com>
2023-06-05 12:50:24 -0300

    s

---
+++ HTML5-CSS3/Mypage/script.js
@@ -0,0 +1,11 @@
+function showDescript(x){
+
+    document.getElementById('tecnologyBelow').style.display = 'none'
+    document.getElementById('langHTML').style.display = 'none'
+    document.getElementById('langCSS').style.display = 'none'
+    document.getElementById('langJAVASCRIPT').style.display = 'none'
+    document.getElementById('langREACTJS').style.display = 'none'
+
+    x.style.display = 'block'
+
+}
