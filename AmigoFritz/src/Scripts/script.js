1c108662bd26db0dc3f6b8d130c83f3bb3b847af
Henrique Griebeler <henruqiedearajogriebelerr@yahoo.com>
2024-01-11 23:53:25 -0300

    a
    a

--- AmigoFritz/src/Scripts/script.js
+++ AmigoFritz/src/Scripts/script.js
@@ -1,9 +1,39 @@
 let id = 1;
 
-function addNewClote(){
+function adicionarItem() {
+    var novoItem = document.getElementById("inputItem").value;
 
-const text = document.getElementById(id).value;
-id+=1
-document.getElementById('clotes').innerHTML+= "<br><input type='text' name='Roupas' id="+id+">"
+    if (novoItem.trim() !== "") {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(novoItem));
 
+        var btnEditar = document.createElement("button");
+        btnEditar.appendChild(document.createTextNode("Editar"));
+        btnEditar.onclick = function() { editarItem(li); };
+
+        var btnExcluir = document.createElement("button");
+        btnExcluir.appendChild(document.createTextNode("Excluir"));
+        btnExcluir.onclick = function() { excluirItem(li); };
+
+        li.appendChild(btnEditar);
+        li.appendChild(btnExcluir);
+
+        document.getElementById("listaItens").appendChild(li);
+        document.getElementById("inputItem").value = "";
+    } else {
+        alert("Ops, faltou digitar qual a roupa a ser doada!");
+    }
+}
+
+function editarItem(item) {
+    var novoTexto = prompt("Digite o novo texto:", item.firstChild.nodeValue);
+    if (novoTexto !== null) {
+        item.firstChild.nodeValue = novoTexto;
+    }
+}
+
+function excluirItem(item) {
+    
+        item.parentNode.removeChild(item);
+    
 }
