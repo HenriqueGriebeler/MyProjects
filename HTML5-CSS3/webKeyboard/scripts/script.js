d518ec9ba8b8754e5639bf5998c3ebcb435a693b
Henrique Griebeler <henruqiedearajogriebelerr@yahoo.com>
2024-03-17 15:48:11 -0300

    Web Keyboard 1.0
    Finally the first version of Web Keyboard is released!

--- HTML5-CSS3/webKeyboard/scripts/script.js
+++ HTML5-CSS3/webKeyboard/scripts/script.js
@@ -1,6 +1,133 @@
-
 function keyboard(){ 
 
   document.getElementById('keyboardBody').style.display = 'block';
   
 }
+
+text = document.getElementById('text')
+keys = []
+
+function type(a){
+
+if(a == 'invertedBar'){
+  keys.push(String.fromCharCode(92)); //93
+  text.value = keys.join('')
+} else if(a == 'rightBracket'){
+  keys.push(String.fromCharCode(93));
+  text.value = keys.join('')
+} else if(a == 'leftBracket'){
+  keys.push(String.fromCharCode(91));
+  text.value = keys.join('')
+} else if(a == 'accent'){
+  keys.push(String.fromCharCode(39));
+  text.value = keys.join('')
+
+} else{
+if(shift == false && capsLock == false){
+  keys.push(a.toLowerCase())
+}else{
+  keys.push(a.toUpperCase())
+}
+text.value = keys.join('')
+if(capsLock == false && shift == true){
+shiftOff()
+}
+}
+} 
+
+function clean(){
+
+  keys = []
+  text.value = keys.join('')
+
+} function enter(){
+keys.push('\n')
+text.value = keys.join('');
+
+} function Bks(){
+
+keys.pop()
+text.value = keys.join('');
+
+} 
+
+let shift = false;
+
+function shiftOn(){
+  
+  //Troca a cor do botão shift
+ if(shift == false){ 
+  capsLock = false
+  capsOff()
+  shift = true;
+document.getElementById('shift').style.backgroundColor = '#4e4e4e'
+
+  // Seleciona todos os elementos com a classe 'normalKey'
+var allNormalKeys = document.querySelectorAll('.normalKeyPosition');
+
+allNormalKeys.forEach(function(allNormalKey) {
+    
+  allNormalKey.textContent = allNormalKey.textContent.toUpperCase();
+
+})} else { 
+
+ shiftOff() 
+  
+}
+} function shiftOff(){
+
+  shift = false
+  capsLock = false
+  capsOff()
+  //Faz a cor do botão voltar ao normal
+  document.getElementById('shift').style.backgroundColor = '#333333'
+
+var allNormalKeys = document.querySelectorAll('.normalKeyPosition');
+
+allNormalKeys.forEach(function(allNormalKey) {
+    
+  allNormalKey.textContent = allNormalKey.textContent.toLowerCase();
+})
+} 
+capsLock = false
+
+ function caps(){
+
+  if(capsLock == false){
+    capsOn()
+    capsLock = true;
+  } else {
+    capsOff()
+    capsLock = false;
+  }
+
+ }
+
+ function capsOn(){
+
+  shiftOff()
+  shift = false
+    document.getElementById('caps').style.backgroundColor = '#4e4e4e'
+
+    // Seleciona todos os elementos com a classe 'normalKey'
+  var allNormalKeys = document.querySelectorAll('.normalKeyPosition');
+  
+  allNormalKeys.forEach(function(allNormalKey) {
+      
+    allNormalKey.textContent = allNormalKey.textContent.toUpperCase();  
+
+  })}
+
+  function capsOff(){
+  
+    //shift = false
+    //Faz a cor do botão voltar ao normal
+    document.getElementById('caps').style.backgroundColor = '#333333'
+  
+  var allNormalKeys = document.querySelectorAll('.normalKeyPosition');
+  
+  allNormalKeys.forEach(function(allNormalKey) {
+      
+    allNormalKey.textContent = allNormalKey.textContent.toLowerCase();
+  })
+  }
