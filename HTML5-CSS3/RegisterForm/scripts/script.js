8b6a0a57a1511feb06ca439f0c96333d309a72e2
Henrique Griebeler <106834251+IkoRico@users.noreply.github.com>
2025-03-30 10:46:24 -0300

    Form page update
    I've started the forms on the page

--- HTML5-CSS3/RegisterForm/scripts/script.js
+++ HTML5-CSS3/RegisterForm/scripts/script.js
@@ -2,4 +2,5 @@ console.log('Página carregada com sucesso')
 
 function openForm(){
     document.getElementById('mainForm').style.display = 'block';
+    document.getElementById('blurMain').style.filter = 'blur(3px)';
 }
