f84ce14a53748b034b6fefc18b69a21327b4ea3d
Henrique Griebeler <106834251+IkoRico@users.noreply.github.com>
2025-04-02 07:58:52 -0300

    Error message
    Error message for Name label ready.

--- HTML5-CSS3/RegisterForm/scripts/script.js
+++ HTML5-CSS3/RegisterForm/scripts/script.js
@@ -10,29 +10,30 @@ const age = document.getElementById('age');
 const date = document.getElementById('date');
 const email = document.getElementById('email');
 
- /*username.addEventListener('keydown', (a) => {
-        console.log(a.key)
-    if(!/^[a-zA-Z]$/.test(a.key) && a.key != 'Shift' && a.key != ' ' && a.key != 'Backspace' && a.key != 'Dead' && a.key != 'é' && a.key != 'ç' && a.key != 'ú' && a.key != 'á' && a.key != 'ã'){ //TESTA SE O QUE FOI DIGITADO ESTA ENTRE a-z e A-Z
-        console.log('Apenas letras!')
-        document.getElementById('name_message_error').style.visibility = 'visible';
-    
-    }
-        
-})*/
 
 function nameProtect(){
 console.log('Função chamada')
-console.log(username.value)
-console.log(username.value.size)
-/*for(let i = 0; i < username.size; i++){
-    console.log('For iniciado')
-    if(!isNaN(username[i])){
-        alert('DETECTADA COISA ERRADA')
-    }
+console.log(username.value.length)
+for(let i = 0; i < username.value.length; i++){
     
-}*/
+    if(!isNaN(username.value[i]) || username.value[i] == '-' || username.value[i] == '=' || username.value[i] == '´' || username.value[i] == ','){
+        errorInput(username, 'Preencha apenas com letras...')
+    console.log('errou')}
+    
+}
 
 }
 
+
+function errorInput(input, message){
+
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("span");
+
+    textMessage.innerText = message;
+
+    textMessage.className = 'error-message'
+
+}
 
 
