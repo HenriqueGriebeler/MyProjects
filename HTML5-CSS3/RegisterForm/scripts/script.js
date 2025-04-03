6baf666572f54129cebe0bb01a223dc983a92898
Henrique Griebeler <106834251+IkoRico@users.noreply.github.com>
2025-04-03 08:04:02 -0300

    Improving name and email field
    Improving security checks for name and email.

--- HTML5-CSS3/RegisterForm/scripts/script.js
+++ HTML5-CSS3/RegisterForm/scripts/script.js
@@ -11,19 +11,38 @@ const date = document.getElementById('date');
 const email = document.getElementById('email');
 
 
-function nameProtect(){
-console.log('Função chamada')
-console.log(username.value.length)
-for(let i = 0; i < username.value.length; i++){
+function protectFields(currentField, message){
+let errorMessage = false
+let atCount = 0
+console.log(currentField.value.length)
+
+
+for(let i = 0; i < currentField.value.length; i++){ //Begin the read of letter by letter
     
-    if(!isNaN(username.value[i]) || username.value[i] == '-' || username.value[i] == '=' || username.value[i] == '´' || username.value[i] == ','){
-        errorInput(username, 'Preencha apenas com letras...')
-    console.log('errou')}
-    
+if(currentField === username){
+    if(!isNaN(currentField.value[i]) || currentField.value[i] == '-' || currentField.value[i] == '=' || currentField.value[i] == '´' || currentField.value[i] == ','){
+        errorMessage = true
+}
+
+else if(currentField === email){
+    if(currentField.value[i] == '@'){
+        atCount++
+    }
 }
 
 }
 
+if(currentField === email && atCount > 1 || currentField === email && atCount == 0){
+    errorMessage = true;
+}
+console.log(errorMessage)
+if(errorMessage){
+    errorInput(currentField, message)
+} else{
+    correctInput(currentField)
+}
+}
+}
 
 function errorInput(input, message){
 
@@ -33,6 +52,19 @@ function errorInput(input, message){
     textMessage.innerText = message;
 
     textMessage.className = 'error-message'
+
+}
+
+function correctInput(input){
+
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("span");
+
+    textMessage.innerText = '';
+
+    textMessage.className = 'no-error-message'
+
+
 
 }
 
